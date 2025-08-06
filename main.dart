eab429a386cbd9856d7c009e58464b5a264eb7d9
Shammah_BIAnalyst <151569483+aqualinqs@users.noreply.github.com>
2025-08-06 16:14:45 +0000

    Update main.dart

--- main.dart
+++ main.dart
@@ -23,10 +23,10 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         '/': (context) => const HomePage(),
-        '/tech_journey': (context) => TechJourney(),
-        '/profile': (context) => Profile(),
-        '/projects': (context) => Projects(),
-        '/connect': (context) => Connect(),
+        '/tech_journey': (context) => const TechJourney(),
+        '/profile': (context) => const Profile(),
+        '/projects': (context) => const Projects(),
+        '/connect': (context) => const Connect(),
       },
     );
   }
@@ -50,154 +50,143 @@ class HomePage extends StatelessWidget {
       appBar: buildAppBar(context),
       body: LayoutBuilder(
         builder: (context, constraints) {
-          final screenSize = MediaQuery.of(context).size;
           final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+          final screenWidth = constraints.maxWidth;
+          final padding = screenWidth < 600 ? 16.0 : 80.0;
 
-          final fontSizeMultiplier = screenSize.width / 400;
-
-          return SingleChildScrollView(
-            padding: EdgeInsets.symmetric(
-              horizontal: screenSize.width * 0.05,
-              vertical: screenSize.height * 0.05,
-            ),
+          return Padding(
+            padding: EdgeInsets.all(padding),
             child: Flex(
               direction: isPortrait ? Axis.vertical : Axis.horizontal,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Flexible(
-                  flex: 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome! To My World of Creativity',
-                        style: TextStyle(
-                          fontSize: 20 * fontSizeMultiplier,
-                          color: Colors.cyan,
-                          shadows: [
-                            const Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 3.0,
-                              color: Colors.lightBlue,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.01),
-                      Text(
-                        'SHAMMAH AGYARE',
-                        style: TextStyle(
-                          fontSize: 26 * fontSizeMultiplier,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.cyan,
-                          shadows: [
-                            const Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 3.0,
-                              color: Colors.lightBlue,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.02),
-                      Text(
-                        'Consulting Analyst | Environmental Researcher | UI/UX Designer | Product Developer',
-                        style: TextStyle(
-                          fontSize: 14 * fontSizeMultiplier,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          shadows: [
-                            const Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 1.0,
-                              color: Colors.white24,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.02),
-                      Text(
-                        'Passionate about making sustainable impacts worldwide through technology, design and systems thinking. '
-                        'Proficient in SQL, Python, Excel, STATA, Power BI, Tableau, Cloud Computing, GIS, HTML, Flutter, Django, Figma, MIS & CRM systems. '
-                        'Looking for a BI Analyst who can transform your "UGH-OH!" INTO WOW-AHA!',
-                        style: TextStyle(
-                          fontSize: 12 * fontSizeMultiplier,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.02),
-                      Text(
-                        "LET'S CONNECT TO UNLOCK THIS MYSTERY!",
-                        style: TextStyle(
-                          fontSize: 14 * fontSizeMultiplier,
-                          color: Colors.white,
-                          shadows: [
-                            const Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 3.0,
-                              color: Colors.white10,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.02),
-                      Wrap(
-                        spacing: 10,
-                        runSpacing: 10,
-                        children: [
-                          IconButton(
-                              onPressed: () => _launchURL('https://github.com/aqualinqs/aqualinqs'),
-                              icon: Image.asset('assets/icons/github.png', width: 30, height: 30)),
-                          IconButton(
-                              onPressed: () => _launchURL('https://linkedin.com/in/shammah-agyare'),
-                              icon: Image.asset('assets/icons/linkedin.png', width: 25, height: 25)),
-                          IconButton(
-                              onPressed: () => _launchURL('https://wa.me/233558457631'),
-                              icon: Image.asset('assets/icons/whatsApp.png', width: 25, height: 25)),
-                          IconButton(
-                              onPressed: () => _launchURL('https://www.instagram.com/ms.shammah/'),
-                              icon: Image.asset('assets/icons/insta.png', width: 25, height: 25)),
-                          IconButton(
-                              onPressed: () => _launchURL(
-                                  'https://mail.google.com/mail/?view=cm&to=agyareshammah@gmail.com&su=[Type subject]&body=[Type message here and send]'),
-                              icon: Image.asset('assets/icons/message2.png', width: 25, height: 25)),
-                        ],
-                      ),
-                      SizedBox(height: screenSize.height * 0.02),
-                      Text(
-                        "OR FIRST EXPLORE FOR MORE!",
-                        style: TextStyle(
-                          fontSize: 14 * fontSizeMultiplier,
-                          color: Colors.white,
-                          shadows: [
-                            const Shadow(
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 3.0,
-                              color: Colors.white10,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Center(
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.keyboard_arrow_down_rounded,
-                            color: Colors.white,
-                            size: 40,
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome! To My World of Creativity',
+                          style: TextStyle(
+                            fontSize: isPortrait ? 28 : 40,
+                            color: Colors.cyan,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 3.0,
+                                color: Colors.lightBlue,
+                              ),
+                            ],
                           ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/');
-                          },
                         ),
-                      ),
-                    ],
+                        Text(
+                          'SHAMMAH AGYARE',
+                          style: TextStyle(
+                            fontSize: isPortrait ? 35 : 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.cyan,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 3.0,
+                                color: Colors.lightBlue,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            'Consulting Analyst | Environmental Researcher | UI/UX Designer | Product Developer',
+                            style: TextStyle(
+                              fontSize: isPortrait ? 16 : 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 1.0,
+                                  color: Colors.white24,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            'Passionate about making sustainable impacts worldwide through technology, design and systems thinking. Proficient in SQL, Python, Excel, STATA, Power BI, Tableau, Cloud Computing, GIS, HTML, Flutter, Django, Figma, MIS & CRM systems. Looking for a BI Analyst who can transform your "UGH-OH!" INTO WOW-AHA!',
+                            style: TextStyle(
+                              fontSize: isPortrait ? 13 : 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            "LET'S CONNECT TO UNLOCK THIS MYSTERY!",
+                            style: TextStyle(
+                              fontSize: isPortrait ? 13 : 15,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 3.0,
+                                  color: Colors.white10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Wrap(
+                          spacing: 8.0,
+                          runSpacing: 8.0,
+                          children: [
+                            IconButton(onPressed: () => _launchURL('https://github.com/aqualinqs/aqualinqs'), icon: Image.asset('assets/icons/github.png', width: 40, height: 40)),
+                            IconButton(onPressed: () => _launchURL('https://linkedin.com/in/shammah-agyare'), icon: Image.asset('assets/icons/linkedin.png', width: 30, height: 30)),
+                            IconButton(onPressed: () => _launchURL('https://wa.me/233558457631'), icon: Image.asset('assets/icons/whatsApp.png', width: 30, height: 30)),
+                            IconButton(onPressed: () => _launchURL('https://www.instagram.com/ms.shammah/'), icon: Image.asset('assets/icons/insta.png', width: 30, height: 30)),
+                            IconButton(onPressed: () => _launchURL('https://mail.google.com/mail/?view=cm&to=agyareshammah@gmail.com&su=[Type subject]&body=[Type message here and send]'), icon: Image.asset('assets/icons/message2.png', width: 30, height: 30)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            "OR FIRST EXPLORE FOR MORE!",
+                            style: TextStyle(
+                              fontSize: isPortrait ? 13 : 15,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  offset: Offset(1.0, 1.0),
+                                  blurRadius: 3.0,
+                                  color: Colors.white10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: IconButton(
+                              icon: const Icon(Icons.keyboard_arrow_down_rounded,
+                                color: Colors.white,
+                                size: 40,
+                              ),
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/');
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(width: isPortrait ? 0 : screenSize.width * 0.05, height: isPortrait ? screenSize.height * 0.05 : 0),
-                const Flexible(
-                  flex: 1,
-                  child: ProfileImage(),
-                ),
+                if (!isPortrait)
+                  const ProfileImage(),
               ],
             ),
           );
@@ -211,12 +200,12 @@ class HomePage extends StatelessWidget {
       title: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextButton(
-          onPressed: () => Navigator.pushNamed(context, '/'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/');
+          },
           child: const Text(
             "S'Tech Portfolio",
             style: TextStyle(
-              fontSize: 20,
-              color: Colors.blueAccent,
               shadows: [
                 Shadow(
                   offset: Offset(1.0, 1.0),
@@ -224,27 +213,49 @@ class HomePage extends StatelessWidget {
                   color: Colors.blueGrey,
                 ),
               ],
+              fontSize: 20,
+              color: Colors.blueAccent,
             ),
           ),
         ),
       ),
       actions: [
-        TextButton(
-            onPressed: () => Navigator.pushNamed(context, '/tech_journey'),
-            child: const Text('Tech Journey',
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent))),
-        TextButton(
-            onPressed: () => Navigator.pushNamed(context, '/projects'),
-            child: const Text('Projects',
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent))),
-        TextButton(
-            onPressed: () => Navigator.pushNamed(context, '/profile'),
-            child: const Text('Profile',
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent))),
-        TextButton(
-            onPressed: () => Navigator.pushNamed(context, '/connect'),
-            child: const Text('Connect',
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent))),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/tech_journey');
+            },
+            child: const Text('Tech Journey', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/projects');
+            },
+            child: const Text('Projects', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+            child: const Text('Profile', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/connect');
+            },
+            child: const Text('Connect', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+          ),
+        ),
       ],
     );
   }
@@ -255,14 +266,11 @@ class ProfileImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final imgSize = screenSize.width * 0.4;
-
     return Container(
       decoration: BoxDecoration(
         color: Colors.cyanAccent,
         borderRadius: BorderRadius.circular(150),
-        boxShadow: const [
+        boxShadow: [
           BoxShadow(
             color: Colors.cyan,
             spreadRadius: 1.0,
@@ -274,11 +282,12 @@ class ProfileImage extends StatelessWidget {
         borderRadius: BorderRadius.circular(150),
         child: Image.asset(
           'assets/images/profilepic.jpg',
-          height: imgSize,
-          width: imgSize * 0.77,
+          height: 350,
+          width: 270,
           fit: BoxFit.cover,
         ),
       ),
     );
   }
 }
+
